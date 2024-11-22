808213e9a85867a79c6674c0d4df89bdebbf9388
mohameddek <mohameddek366@gmail.com>
2024-11-21 21:11:19 -0500

    fixed agin

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Footer } from "./Components/Footer/Footer";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { LoginSignup } from "./Pages/LoginSignup";
 import { Product } from "./Pages/Product";
-import { Shop } from "./Pages/shop";
+import { Shop } from "./Pages/Shop";
 import { ShopCategory } from "./Pages/ShopCategory";
 import { Cart } from "./Pages/Cart";
 import men_banner from '/Frontend_Assets/banner_mens.png'
